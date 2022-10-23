5067f21c1014d04d3f7453b2411369c54188e20a
Ogliss <randomdragon2@hotmail.com>
2022-10-23 10:39:25 +0100

    initial 1.4 update

---
+++ 1.4/Source/Harvest Everything/Harmony/Patches/MedicalRecipesUtility_IsClean_Patch.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using System.Collections.Generic;
+using System;
+using Verse.AI;
+using System.Text;
+using System.Linq;
+using Verse.AI.Group;
+using RimWorld.Planet;
+using UnityEngine;
+
+namespace Harvest_Everything
+{
+    [HarmonyPatch(typeof(MedicalRecipesUtility), "IsClean")]
+    public static class MedicalRecipesUtility_IsClean_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn pawn, BodyPartRecord part, ref bool __result)
+        {
+            if (__result)
+            {
+                __result = !HarmonyPatches.IsChildrenDamaged(pawn, part);
+            }
+        }
+    }
+}
---
+++ 1.4/Source/Harvest Everything/Harmony/Patches/Recipe_InstallNaturalBodyPart_GetPartsToApplyOn_Patch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+
+namespace Harvest_Everything
+{
+    public class Recipe_InstallNaturalBodyPart_GetPartsToApplyOn_Patch
+    {
+        public static IEnumerable<BodyPartRecord> Postfix(IEnumerable<BodyPartRecord> __result, Pawn pawn, RecipeDef recipe)
+        {
+            foreach (BodyPartRecord part in __result)
+            {
+                if (!HarmonyPatches.IsChildrenDamaged(pawn, part))
+                {
+                    continue;
+                }
+                yield return part;
+            }
+
+            IEnumerable<BodyPartRecord> notMissingParts = pawn.health.hediffSet.GetNotMissingParts(BodyPartHeight.Undefined, BodyPartDepth.Undefined, null, null).Where(x => x.def.spawnThingOnRemoved != null && recipe.appliedOnFixedBodyParts.Contains(x.def));
+            foreach (BodyPartRecord part in notMissingParts)
+            {
+                if (!HarmonyPatches.IsChildrenDamaged(pawn, part))
+                {
+                    continue;
+                }
+                yield return part;
+            }
+            yield break;
+        }
+        
+    }
+}
